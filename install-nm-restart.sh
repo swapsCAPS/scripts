27cb0494d67c01791df2e40263fae6095381510f
swapsCAPS <verstegen.daan@gmail.com>
2018-06-10 13:05:40 +0200

    hacky but more straight forward method. ugh systemd : /

---
+++ install-nm-restart.sh
@@ -0,0 +1,2 @@
+sudo cp ./restart-nm.sh /lib/systemd/system-sleep/.
+sudo chmod +x /lib/systemd/system-sleep/restart-nm.sh
--- restart-nm.sh
+++ restart-nm.sh
@@ -1,2 +1,5 @@
-echo "Restarting NetworkManager"
-sudo systemctl restart NetworkManager.service
+#!/bin/sh
+if [ "${1}" == "post" ]; then
+  echo "Restarting NetworkManager"
+  sudo systemctl restart NetworkManager.service
+fi
