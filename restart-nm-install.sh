6c7450c7248ac58da75c78cac2b535f3783fb73f
swapsCAPS <verstegen.daan@gmail.com>
2018-06-10 12:24:24 +0200

    generate absolute path at install

--- restart-nm-install.sh
+++ restart-nm-install.sh
@@ -1,2 +1,19 @@
-sudo cp ./restart-nm.service /etc/systemd/system/.
+sudo rm    /etc/systemd/system/restart-nm.service
+sudo touch /etc/systemd/system/restart-nm.service
+echo "
+[Unit]
+Description=Restarts NetworkManager after suspend to make wifi work again... Ugh
+After=suspend.target
+
+[Service]
+User=root
+Type=oneshot
+ExecStart=$HOME/src/scripts/restart-nm.sh
+TimeoutSec=0
+StandardOutput=syslog
+
+[Install]
+WantedBy=suspend.target
+" | sudo tee -a /etc/systemd/system/restart-nm.service
+
 sudo systemctl enable restart-nm
